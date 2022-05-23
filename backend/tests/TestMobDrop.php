b6562c7175ffc558c31f02e3594851784879372c
Iaroslav Os <osmnbx@gmail.com>
2022-05-23 21:36:34 +0300

    unit updated + 1 test

--- backend/tests/TestMobDrop.php
+++ backend/tests/TestMobDrop.php
@@ -1,1 +1,16 @@
 <?php
+
+require_once '../classes/MobDrop.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TestMobDrop extends TestCase
+{
+    function test_mobdrop()
+    {
+        $url = 'http://l2-informer/backend/mobdrop';
+        $result = file_get_contents($url);
+        $response = file_get_contents('./files/test_mobdrop.json');
+        $this->assertEquals($response, $result);
+    }
+}
