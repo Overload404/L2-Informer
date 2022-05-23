a52d19d459c7d12a2557214d9b8353fe996342c0
Iaroslav Os <osmnbx@gmail.com>
2022-05-23 19:42:01 +0300

    +per_page & $default page

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,10 @@
 <?php
 
-$db = new PDO('sqlite:./data/main.db');
+// Enable CORS from React 
 header("Access-Control-Allow-Origin: *");
+// DataBase connection
+$db = new PDO('sqlite:./data/main.db');
+// Max rows per page
+$per_page = 100;
+// Default page is 1
+$page = $_GET['page'] ?? 1;
