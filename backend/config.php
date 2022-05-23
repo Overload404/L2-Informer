73ca2cecb4e839b86766c4d84d439013bc340f89
Iaroslav Os <osmnbx@gmail.com>
2022-05-23 22:45:19 +0300

    updated, moved per_page + def.page > core General

---
+++ backend/config.php
@@ -0,0 +1,7 @@
+<?php
+// Enable CORS from React 
+header("Access-Control-Allow-Origin: *");
+// DataBase connection
+$db = new PDO('sqlite:./data/main.db');
+// Include var_dump++
+require_once 'Tools.php';
