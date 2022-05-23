0c51273bf416534a289a9b5c61a5d3702c6ae0b8
Iaroslav Os <osmnbx@gmail.com>
2022-05-23 19:31:44 +0300

    upd: added general test, response to json

--- backend/tests/TestMobs.php
+++ backend/tests/TestMobs.php
@@ -9,9 +9,15 @@ class TestMobs extends TestCase
     function test_page_weakpoint()
     {
         $url = 'http://l2-informer/backend/mobs?page=1&weakpoint=5598';
-        $url_content = file_get_contents($url);
+        $result = file_get_contents($url);
         $response = '{"response":[],"pagination":{"total":0,"lastPage":0,"currentPage":"1","from":0,"to":0}}';
-
-        $this->assertEquals($response, $url_content);
+        $this->assertEquals($response, $result);
+    }
+    function test_mobs()
+    {
+        $url = 'http://l2-informer/backend/mobs';
+        $result = file_get_contents($url);
+        $response = file_get_contents('./TestMobs.json');
+        $this->assertEquals($response, $result);
     }
 }
