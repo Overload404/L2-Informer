f9364a753d60f0897035ed8ae91808ec088978fa
Iaroslav Os <osmnbx@gmail.com>
2022-05-23 22:47:44 +0300

    added new tests

---
+++ backend/tests/TestCore.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once '../core.php';
+
+class TestCore extends TestCase
+{
+    function test_mobs()
+    {
+        $url = 'http://l2-informer/backend/mobs';
+        $result = file_get_contents($url);
+        $response = file_get_contents('./files/test_mobs.json');
+        $this->assertEquals($response, $result);
+    }
+
+    function test_mobs_page_weakpoint()
+    {
+        $url = 'http://l2-informer/backend/mobs?page=1&weakpoint=5598';
+        $result = file_get_contents($url);
+        $response = '{"response":[],"pagination":{"total":0,"lastPage":0,"currentPage":"1","from":0,"to":0}}';
+        $this->assertEquals($response, $result);
+    }
+
+    function test_mobdrop()
+    {
+        $url = 'http://l2-informer/backend/mobdrop';
+        $result = file_get_contents($url);
+        $response = file_get_contents('./files/test_mobdrop.json');
+        $this->assertEquals($response, $result);
+    }
+    function test_mobdrop_npcid_18001()
+    {
+        $url = 'http://l2-informer/backend/mobdrop?npcid=18001';
+        $result = file_get_contents($url);
+        $response = file_get_contents('./files/test_mobdrop_npcid_18001.json');
+        $this->assertEquals($response, $result);
+    }
+}
