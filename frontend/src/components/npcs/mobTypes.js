4aa353151ee76e4f453adb8f5d7687ffde6cc789
Overload404 <myhulu@ex.ua>
2022-05-23 16:36:32 +0300

    added mobTypes.js to connect mobTable to backend

---
+++ frontend/src/components/npcs/mobTypes.js
@@ -0,0 +1,11 @@
+let bosses = '"L2RaidBoss","L2GrandBoss"';
+
+let npcs =
+  '"L2WeddingManager","L2Npc","L2SiegeNpc","L2NpcWalker","L2NpcBuffer","L2BabyPet","L2TvTEventNpc","L2Pet","L2TamedBeast","L2ControlTower","L2EffectPoint","L2SiegeSummon","L2FeedableBeast","L2Auctioneer","L2Observation","L2FestivalGuide","L2CabaleBuffer","L2ClassMaster","L2FriendlyMob","L2SymbolMaker","L2TownPet","L2SignsPriest","L2Warehouse","L2Trainer","L2VillageMaster","L2TransformManager","L2Guard","L2Adventurer","L2Fisherman","L2Merchant","L2Deco","L2Artefact","L2CastleTeleporter","L2CastleBlacksmith","L2CastleWarehouse","L2CastleChamberlain","L2MercManager","L2Teleporter","L2ClanHallManager","L2Doormen","L2WyvernManager","L2CastleMagician","L2Commander","L2SiegeGuard","L2FortMerchant","L2OlympiadManager","L2ManorManager"';
+
+let mobs =
+  '"L2Chest","L2Minion","L2RiftInvader","L2Monster","L2FestivalMonster","L2PenaltyMonster","L2Decoy"';
+let all =
+  '"L2Chest","L2Minion","L2RiftInvader","L2Monster","L2FestivalMonster","L2PenaltyMonster","L2Decoy","L2WeddingManager","L2Npc","L2SiegeNpc","L2NpcWalker","L2NpcBuffer","L2BabyPet","L2TvTEventNpc","L2Pet","L2TamedBeast","L2ControlTower","L2EffectPoint","L2SiegeSummon","L2FeedableBeast","L2Auctioneer","L2Observation","L2FestivalGuide","L2CabaleBuffer","L2ClassMaster","L2FriendlyMob","L2SymbolMaker","L2TownPet","L2SignsPriest","L2Warehouse","L2Trainer","L2VillageMaster","L2TransformManager","L2Guard","L2Adventurer","L2Fisherman","L2Merchant","L2Deco","L2Artefact","L2CastleTeleporter","L2CastleBlacksmith","L2CastleWarehouse","L2CastleChamberlain","L2MercManager","L2Teleporter","L2ClanHallManager","L2Doormen","L2WyvernManager","L2CastleMagician","L2Commander","L2SiegeGuard","L2FortMerchant","L2OlympiadManager","L2ManorManager","L2RaidBoss","L2GrandBoss"';
+
+export default { npcs, mobs, bosses, all };
