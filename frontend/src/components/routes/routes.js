c49e9abef8dc3330054f8272c39da5cd147fcab4
Overload404 <myhulu@ex.ua>
2022-05-26 01:20:25 +0300

    added linkbuilder into routes.js

---
+++ frontend/src/components/routes/routes.js
@@ -0,0 +1,36 @@
+const routes = {
+  backendServer: "http://l2-informer/backend/",
+  mobs: "mobs?",
+  mobDrops: "mobdrop?",
+  mobSkills: "mobskills?",
+  items: "items?",
+  name: "&name=",
+  page: "page=",
+  weakpoint: "&weakpoint=",
+  type: "&type=",
+  npcid: "&npcid=",
+};
+
+const linkBuilder = ({ page, npcid, weakpoint, type, name, ep } = {}) => {
+  let pageRequest = !page || page === "" ? "" : `${routes.page}${page}`;
+  let typeRequest = !type || type === "" ? "" : `${routes.type}${type}`;
+  let weakpointRequest =
+    !weakpoint || weakpoint === "" ? "" : `${routes.weakpoint}${weakpoint}`;
+  let nameRequest = !name || name === "" ? "" : `${routes.name}${name}`;
+  let npcidRequest = !npcid || npcid === "" ? "" : `${routes.npcid}${npcid}`;
+  let epRequest =
+    ep === "mobs"
+      ? routes.mobs
+      : ep === "mobdrops"
+      ? routes.mobDrops
+      : ep === "items"
+      ? routes.items
+      : ep === "mobskills"
+      ? routes.mobSkills
+      : "";
+
+  let URL = `${routes.backendServer}${epRequest}${pageRequest}${weakpointRequest}${npcidRequest}${nameRequest}${typeRequest}`;
+  return URL;
+};
+
+export default linkBuilder;
